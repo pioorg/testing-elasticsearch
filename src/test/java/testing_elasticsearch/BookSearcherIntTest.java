6b9fe6917aae3aa6dd7c577a336c81b56cc9c7e3
Piotr <23506256+pioorg@users.noreply.github.com>
2024-07-19 16:51:27 +0200

    Added one more test.

--- src/test/java/testing_elasticsearch/BookSearcherIntTest.java
+++ src/test/java/testing_elasticsearch/BookSearcherIntTest.java
@@ -149,4 +149,12 @@ public class BookSearcherIntTest {
         var list = systemUnderTest.mostPublishedAuthorsInYears(1800, 2010);
         Assertions.assertEquals("Beatrix Potter", list.get(12).author(), "Beatrix Potter was 13th most published author between 1800 and 2010");
     }
+
+    @Test
+    void shouldFetchTheNumberOfBooksPublishedInGivenYear() {
+        var systemUnderTest = new BookSearcher(client);
+        int books = systemUnderTest.numberOfBooksPublishedInYear(1776);
+        Assertions.assertEquals(2, books, "there were 2 books published in 1776 in the dataset");
+        // or maybe just one? ;-)
+    }
 }
